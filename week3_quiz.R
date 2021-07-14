317a6c032165b5252d3bf9da8713e4c6f0089340
mariakavanagh25 <mariakavanagh25@gmail.com>
2021-07-14 19:24:21 +0100

    updated question 3 - loaded correct file for edu

--- week3_quiz.R
+++ week3_quiz.R
@@ -36,3 +36,43 @@ download.file(file, destfile = "q2.jpg", mode ="wb")
 q2 <- readJPEG("q2.jpg", native = TRUE)
 quantile(q2, probs = c(0.3, 0.8))
 
+# Question 3
+# Load the Gross Domestic Product data for the 190 ranked countries in this data set:
+# https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv 
+
+# Load the educational data from this data set:
+
+# https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv
+
+# Match the data based on the country shortcode. How many of the IDs match? Sort the data frame in 
+# descending order by GDP rank (so United States is last). 
+# What is the 13th country in the resulting data frame?
+
+install.packages("tidyr")
+library(tidyr)
+
+# Look at file - data starts on row 6 - skip the first 5 rows, but do not take 1st row as headers
+# Select only the columns with data and select only the ranked countries
+
+file <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
+download.file(file, destfile = "gdp.csv")
+gdp <- read.csv("gdp.csv",skip =5,header=FALSE)
+gdp2 <- select(gdp, c(1,2,4,5)) %>% 
+  rename(CountryCode = V1, Rank_gdp = V2,economy_gdp= V4,gdp = V5) %>% 
+  transform(Rank_gdp = as.numeric(Rank_gdp))%>%
+  filter(Rank_gdp %in% (1:190))
+
+
+
+file2 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
+download.file(file2, destfile = "edu.csv" )
+edu <- read.csv("edu.csv")
+
+#Merge the 2 data frames , set all to FALSE to take only matched records
+merged_df <- merge(gdp2, edu, by="CountryCode", all=FALSE)
+nrow(merged_df)
+
+sort_df <- arrange(merged_df, desc(Rank_gdp))
+sort_df[13,c(1,3)]
+
+
