981b8b6330c7371e6ad97b383b3c447dedb3afb6
mariakavanagh25 <mariakavanagh25@gmail.com>
2021-07-14 09:25:47 +0100

    Question 2 added

--- week2_quiz.R
+++ week2_quiz.R
@@ -29,3 +29,26 @@ json2 = jsonlite::fromJSON(toJSON(json1))
 
 # Subset data.frame
 json2[json2$full_name == "jtleek/datasharing", "created_at"]
+
+
+#Question 2
+#The sqldf package allows for execution of SQL commands on R data frames. 
+#We will use the sqldf package to practice the queries we might send with the dbSendQuery 
+#command in RMySQL. 
+#
+#Download the American Community Survey data and load it into an R object called: acs
+#
+#https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv
+#
+#Which of the following commands will select only the data for the probability weights pwgtp1 with
+#ages less than 50?
+
+
+library(RSQLite)
+library(sqldf)
+
+file <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+download.file(file, destfile = "./q2.csv")
+
+acs <- read.csv("q2.csv")
+sqldf("select pwgtp1 from acs where AGEP < 50")
