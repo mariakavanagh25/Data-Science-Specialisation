efa1f03b390da0b233b0527b378d7c346f434611
mariakavanagh25 <mariakavanagh25@gmail.com>
2021-07-16 10:09:24 +0100

    question 5 added

--- week3_quiz.R
+++ week3_quiz.R
@@ -82,3 +82,10 @@ merged_df%>%
   group_by(Income.Group)%>%
   summarize(avg_gdp =mean(Rank_gdp), .groups='keep')
 
+# Question 5
+# Cut the GDP ranking into 5 separate quantile groups. 
+# Make a table versus Income.Group. How many countries are Lower middle income but among the 38 nations with highest GDP?
+
+merged_df$cut <- cut(merged_df$Rank_gdp, breaks=quantile(merged_df$Rank_gdp, c(0,0.2,0.4,0.6,0.8,1)))     
+table(merged_df$Income.Group,merged_df$cut)
+
