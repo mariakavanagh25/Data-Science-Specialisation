7b82db85ed90cd5ff102ea3d3beee1862bfd453b
mariakavanagh25 <80263681+mariakavanagh25@users.noreply.github.com>
2022-12-12 10:11:54 +0000

    Add files via upload

---
+++ 04. Exploratory Data Analysis/plot1.R
@@ -0,0 +1,25 @@
+## Loading the data
+
+# Load the data set into R, convert data variable to date/Time class
+
+path <- "./exdata_data_household_power_consumption/household_power_consumption.txt"
+raw_data <- read.table(path, sep=';', header=TRUE, na.strings='?')
+raw_data$Date <- as.Date(raw_data$Date, "%d/%m/%Y")
+
+# Subset to only 2007-02-01 and 2007-02-02
+sub_data <- subset(raw_data, raw_data$Date>=as.Date("2007-02-01") & raw_data$Date <= as.Date("2007-02-02"))
+
+# Create DateTime variables (in date/time class) (needed for plot 2)
+sub_data$DateTime <- strptime(paste(sub_data$Date, sub_data$Time), "%Y-%m-%d %H:%M:%S")
+
+## Making Plots
+
+#Plot 1
+par(mfrow=c(1,1))
+hist(sub_data$Global_active_power, 
+     xlab= "Global Active Power (kilowatts)",
+     main = " Global Active Power",
+     col = "red")
+
+dev.copy(png,file="plot1.png",width=480,height=480)
+dev.off()
